ec9bf638c230f81be26cd00834938e0ad0d1122b
Meera Devi Raval <149183760+meera-raval@users.noreply.github.com>
2023-12-26 23:22:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('addToCartBtn').addEventListener('click', function() {
+    alert('Product added to cart!');
+  });
+  
+
+  
+document.getElementById('addToCartBtn').addEventListener('click', function() {
+  const itemPrice = 500; 
+  alert('Item added to cart! Cost: '+ 'Rs'+' '+ itemPrice );
+});
